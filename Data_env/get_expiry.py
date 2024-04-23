52a0586e5764240666c79404f4fedbe5aba0c25d
Vaibhav Mishra <78559133+VaibhavM07@users.noreply.github.com>
2024-04-23 14:49:27 +0530

    inheritance fix in get_expiry

--- Data_env/get_expiry.py
+++ Data_env/get_expiry.py
@@ -5,10 +5,11 @@ import datetime as dt
 
 class get_expiry(data_cleaning):
 
-    def __init__(self,df:pd.DataFrame):
-        self.df = df
+    def __init__(self,*args, **kwargs):
+        super().__init__( *args, **kwargs)
+        self.df = self.get_futures_data()
         self.current_backtest_date = self.df["Timestamp"].iloc[2].date()
-        #self.current_backtest_date = dt.datetime(2019,5,2)-->test
+        #self.current_backtest_date = dt.datetime(2019,5,2)--> testing
     def day_of_week(self):
 
         day_of_week = self.current_backtest_date.weekday()
