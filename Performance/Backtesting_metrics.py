eb9dff6e3cc865b037be1bd07c297879f72552f1
Vaibhav Mishra <78559133+VaibhavM07@users.noreply.github.com>
2024-04-25 19:21:52 +0530

    ret

--- Performance/Backtesting_metrics.py
+++ Performance/Backtesting_metrics.py
@@ -23,19 +23,19 @@ class Backtesting_metrics():
         self.lossmaking_trades = len(self.tradelog) - self.profitable_trades
         self.win_rate = self.profitable_trades / len(self.tradelog)
 
-        print("Number of Profitable Trades:", self.profitable_trades)
-        print("Number of Loss-Making Trades:", self.lossmaking_trades)
-        print("Win Rate:", round(self.win_rate, 2))
+        return ("Number of Profitable Trades:", self.profitable_trades,
+                "Number of Loss-Making Trades:", self.lossmaking_trades,
+                "Win Rate:", round(self.win_rate, 2))
 
     def avg_pnl_per_trade(self):
         self.avg_profit = self.tradelog[self.tradelog['PnL Including Slippage'] > 0]['PnL Including Slippage'].mean()
         self.avg_loss = self.tradelog[self.tradelog['PnL Including Slippage'] < 0]['PnL Including Slippage'].mean()
 
-        print("Average Profit per trade:", round(self.avg_profit, 2))
-        print("Average Loss per trade:", round(self.avg_loss, 2))
+        return ("Average Profit per trade:", round(self.avg_profit, 2),
+                "Average Loss per trade:", round(self.avg_loss, 2))
 
     def ris_reward(self):
         self.avg_pnl_per_trade()
         self.risk_reward = abs(self.avg_profit / self.avg_loss)
-        print("Risk Reward Ratio:", round(self.risk_reward, 2))
+        return ("Risk Reward Ratio:", round(self.risk_reward, 2))
 
