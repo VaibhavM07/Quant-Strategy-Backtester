bfd9bed387e2fc4856c6bb5bab307e4391576c22
Vaibhav Mishra <78559133+VaibhavM07@users.noreply.github.com>
2024-04-25 19:13:25 +0530

    acount for slippage, avgpnl_pertrade, rr

--- Performance/Backtesting_metrics.py
+++ Performance/Backtesting_metrics.py
@@ -3,39 +3,39 @@ import numpy as np
 
 class Backtesting_metrics():
 
-    def __init__(self,lot_size,tradelog :pd.DataFrame):
+    def __init__(self, lot_size, tradelog: pd.DataFrame,slippage:float = 0.001):
         self.tradelog = tradelog
         self.lot_size = lot_size
+        self.slippage = slippage
+        self.tradelog['Entry Including Slippage'] = self.tradelog['Entry Price'] - (self.tradelog['Entry Price'] * self.slippage)
+        self.tradelog['Exit Including Slippage'] = self.tradelog['Exit Price'] + (self.tradelog['Exit Price'] * self.slippage)
+        self.tradelog['PnL Including Slippage'] = self.lot_size * (self.tradelog['Entry Including Slippage'] - self.tradelog['Exit Including Slippage']) - 100
+        self.tradelog['PnL Including Slippage Cumulative Sum'] = self.tradelog['PnL Including Slippage'].cumsum()
+
 
     def PnL(self):
         self.tradelog['PnL'] = self.lot_size * (self.tradelog['Entry Price'] - self.tradelog['Exit Price'])
         self.tradelog['PnL Cumulative Sum'] = self.tradelog['PnL'].cumsum()
         return self.tradelog
 
-    def PnL_inc_slippage(self):
-        #slippage is deducted, as selling at a lower price is a loss
-        self.tradlog['Entry Including Slippage'] = self.tradlog['Entry Price'] - (self.tradlog['Entry Price'] * 0.001)
-        #slippage is added, as buying at a higher price is a loss
-        self.tradlog['Exit Including Slippage'] = self.tradlog['Exit Price'] + (self.tradlog['Exit Price'] * 0.001)
-
-        #pnlaccounting for slippages
-        self.tradlog['PnL Including Slippage'] = self.lot_size * (
-                    self.tradlog['Entry Including Slippage'] - self.tradlog['Exit Including Slippage']) - 100
-
-        # Adding up the slippage accounted pnl and ploting it
-        self.tradlog['PnL Including Slippage Cumulative Sum'] = self.tradlog['PnL Including Slippage'].cumsum()
-
-        return self.tradelog
-
     def win_rate(self):
-        profitable_trades = len(self.tradlog[self.tradlog['PnL Including Slippage'] > 0])
-        lossmaking_trades = len(self.tradlog) - profitable_trades
-        win_rate = profitable_trades / len(self.tradlog)
+        self.profitable_trades = len(self.tradelog[self.tradelog['PnL Including Slippage'] > 0])
+        self.lossmaking_trades = len(self.tradelog) - self.profitable_trades
+        self.win_rate = self.profitable_trades / len(self.tradelog)
 
-        print("Number of Profitable Trades:", profitable_trades)
-        print("Number of Loss-Making Trades:", lossmaking_trades)
-        print("Win Rate:", round(win_rate, 2))
+        print("Number of Profitable Trades:", self.profitable_trades)
+        print("Number of Loss-Making Trades:", self.lossmaking_trades)
+        print("Win Rate:", round(self.win_rate, 2))
 
+    def avg_pnl_per_trade(self):
+        self.avg_profit = self.tradelog[self.tradelog['PnL Including Slippage'] > 0]['PnL Including Slippage'].mean()
+        self.avg_loss = self.tradelog[self.tradelog['PnL Including Slippage'] < 0]['PnL Including Slippage'].mean()
 
+        print("Average Profit per trade:", round(self.avg_profit, 2))
+        print("Average Loss per trade:", round(self.avg_loss, 2))
 
+    def ris_reward(self):
+        self.avg_pnl_per_trade()
+        self.risk_reward = abs(self.avg_profit / self.avg_loss)
+        print("Risk Reward Ratio:", round(self.risk_reward, 2))
 
