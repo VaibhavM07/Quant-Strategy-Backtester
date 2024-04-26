e49ad577917d3ffb0e34c04dd7438d67783af029
Vaibhav Mishra <78559133+VaibhavM07@users.noreply.github.com>
2024-04-27 03:06:34 +0530

    datetime expiry to formatted stirng date for filtering current expiry option/fut data

--- Data_env/get_expiry.py
+++ Data_env/get_expiry.py
@@ -14,13 +14,15 @@ class get_expiry(data_cleaning):
 
         day_of_week = self.current_backtest_date.weekday()
         return day_of_week
+
     def weekly(self):
-        if self.day_of_week()>3:
-            days_to_add = 7-(self.day_of_week()-3)
+        if self.day_of_week() > 3:
+            days_to_add = 7 - (self.day_of_week() - 3)
         else:
-            days_to_add = (3- self.day_of_week())%7
-        target_weekly_exp = self.current_backtest_date+dt.timedelta(days = days_to_add)
-        return target_weekly_exp
+            days_to_add = (3 - self.day_of_week()) % 7
+        target_weekly_exp = self.current_backtest_date + dt.timedelta(days=days_to_add)
+        target_weekly_exp_formatted = target_weekly_exp.strftime('%d%b').upper()
+        return target_weekly_exp_formatted
 
     def monthly(self):
         next_month = self.current_backtest_date.replace(day=28) + dt.timedelta(days=4)
@@ -30,10 +32,12 @@ class get_expiry(data_cleaning):
         while last_day_of_month.weekday() != 3:
             last_day_of_month -= dt.timedelta(days=1)
         third_thursday = last_day_of_month
+        third_thursday = third_thursday.strftime('%d%b').upper()
 
         if third_thursday.month != current_month:
             while third_thursday.month != current_month:
                 third_thursday -= dt.timedelta(days=1)
+                third_thursday = third_thursday.strftime('%d%b').upper()
         return third_thursday
 
 
