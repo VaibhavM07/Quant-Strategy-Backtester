495f9f6a66b436decd5ef72bb463364db668651a
Vaibhav Mishra <78559133+VaibhavM07@users.noreply.github.com>
2024-04-27 03:04:41 +0530

    ticker to string

--- Data_env/Data_cleaning.py
+++ Data_env/Data_cleaning.py
@@ -2,6 +2,9 @@ from Data_env.Data_loading import data_load
 import pandas as pd
 import string
 import datetime as dt
+import glob
+import warnings
+warnings.filterwarnings('ignore')
 
 class data_cleaning():
 
@@ -9,30 +12,31 @@ class data_cleaning():
         self.path = path
         self.ticker = ticker
     def get_data(self):
-        df = data_load(path=self.path)
-        if df.empty:
+        self.df = data_load(path=self.path)
+        if self.df.empty:
             print("No Data found in "+self.path)
         else:
-            df.insert(0, "Timestamp", df["Date"] + " " + df["Time"])
-            df["Timestamp"] = pd.to_datetime(df["Timestamp"], format="%d/%m/%Y %H:%M:%S")
-            df.drop('Date', inplace=True, axis=1)
-            df.drop('Time', inplace=True, axis=1)
-            return df
+            self.df.insert(0, "Timestamp", self.df["Date"] + " " + self.df["Time"])
+            self.df["Timestamp"] = pd.to_datetime(self.df["Timestamp"], format="%d/%m/%Y %H:%M:%S")
+            self.df["Ticker"] = self.df["Ticker"].apply(str)
+            self.df.drop('Date', inplace=True, axis=1)
+            self.df.drop('Time', inplace=True, axis=1)
+            return self.df
     def get_ticker(self):
-        df = self.get_data()
-        df = df[df["Ticker"].apply(lambda a: a[0:len(self.ticker)]==self.ticker)]
-        if df.empty:
+        self.df = self.get_data()
+        self.df = self.df[self.df["Ticker"].apply(lambda a: a[0:len(self.ticker)]==self.ticker)]
+        if self.df.empty:
             print(self.ticker+" data unavailable")
         else:
-            return df
+            return self.df
 
     def get_ticker_call_data(self):
         try:
-            df = self.get_ticker()
-            ticker_calls = df[df["Ticker"].apply(lambda a: a.strip()[-2:] == "CE")]
+            self.df = self.get_ticker()
+            ticker_calls = self.df[self.df["Ticker"].apply(lambda a: a.strip()[-2:] == "CE")]
             if ticker_calls.empty:
                 # Because of data issue
-                ticker_calls = df[df["Ticker"].apply(lambda a: a.strip()[-6:-4] == "CE")]
+                ticker_calls = self.df[self.df["Ticker"].apply(lambda a: a.strip()[-6:-4] == "CE")]
             return ticker_calls
         except KeyError as e:
             print(self.ticker+"'s option data unavailable")
@@ -43,11 +47,11 @@ class data_cleaning():
 
     def get_ticker_put_data(self):
         try:
-            df = self.get_ticker()
-            ticker_puts = df[df["Ticker"].apply(lambda a: a.strip()[-2:] == "PE")]
+            self.df = self.get_ticker()
+            ticker_puts = self.df[self.df["Ticker"].apply(lambda a: a.strip()[-2:] == "PE")]
             if ticker_puts.empty:
                 # Because of data issue
-                ticker_puts = df[df["Ticker"].apply(lambda a: a.strip()[-6:-4] == "PE")]
+                ticker_puts = self.df[self.df["Ticker"].apply(lambda a: a.strip()[-6:-4] == "PE")]
             return ticker_puts
         except KeyError as e:
             print(self.ticker+"'s option data unavailable")
@@ -57,11 +61,11 @@ class data_cleaning():
             return None
     def get_futures_data(self):
         try:
-            df = self.get_ticker()
-            ticker_fut = df[df["Ticker"].apply(lambda a: a.strip()[-2:] == "-I")]
+            self.df = self.get_ticker()
+            ticker_fut = self.df[self.df["Ticker"].apply(lambda a: a.strip()[-2:] == "-I")]
             if ticker_fut.empty:
                 # Because of data issue
-                ticker_fut = df[df["Ticker"].apply(lambda a: a.strip()[-6:-4] == "-I")]
+                ticker_fut = self.df[self.df["Ticker"].apply(lambda a: a.strip()[-6:-4] == "-I")]
             return ticker_fut
         except KeyError as e:
             print(self.ticker+"'s future's data unavailable")
