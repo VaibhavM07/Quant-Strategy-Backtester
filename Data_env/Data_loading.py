d5f665163344a56c3adf7a44c91085b08c30123d
Vaibhav Mishra <78559133+VaibhavM07@users.noreply.github.com>
2024-05-02 12:55:22 +0530

    feature to get all the existing expiries of the dataframe

--- Data_env/Data_loading.py
+++ Data_env/Data_loading.py
@@ -6,9 +6,3 @@ import warnings
 import string
 warnings.filterwarnings('ignore')
 def data_load(path: string)-> pd.DataFrame:
-    global files
-    folder = glob.glob(path)
-    for file in folder:
-        files = glob.glob(file + "/*.csv")
-    df = pd.concat([pd.read_csv(files[i]) for i in range(0, len(files))])
-    return df
--- Data_env/get_expiry.py
+++ Data_env/get_expiry.py
@@ -10,61 +10,54 @@ class get_expiry(data_cleaning):
     def __init__(self,*args, **kwargs):
         super().__init__( *args, **kwargs)
         self.dff = self.get_futures_data()
-        self.current_backtest_date = self.dff["Timestamp"].iloc[2].date()
-        self.india_holidays = holidays.country_holidays(country="IN")
-        #self.current_backtest_date = dt.datetime(2019,5,2)--> testing
-    def day_of_week(self):
+        self.backtest_dates = []
+        if not self.dff.empty:
+            self.current_backtest_date = self.dff["Timestamp"].dt.date.min()
+            self.current_backtest_date_max = self.dff["Timestamp"].dt.date.max()
+        else:
+            self.current_backtest_date = None
+            self.current_backtest_date_max = None
+        if self.current_backtest_date and self.current_backtest_date_max:
+            for curr_date in range((self.current_backtest_date_max - self.current_backtest_date).days + 1):
+                self.backtest_dates.append(self.current_backtest_date + dt.timedelta(days=curr_date))
 
-        day_of_week = self.current_backtest_date.weekday()
-        return day_of_week
+        self.india_holidays = holidays.country_holidays(country="IN")
+
 
     def current_weekly(self):
-        if self.day_of_week() > 3:
-            days_to_add = 7 - (self.day_of_week() - 3)
-        else:
-            days_to_add = (3 - self.day_of_week()) % 7
-        target_weekly_exp = self.current_backtest_date + dt.timedelta(days=days_to_add)
-        if target_weekly_exp in self.india_holidays:
-            print("Thrusday holiday")
-            target_weekly_exp = target_weekly_exp -dt.timedelta(days=1)
-        target_weekly_exp_formatted = target_weekly_exp.strftime('%d%b').upper()
-        return target_weekly_exp_formatted
+        target_weekly_exp_formatted =[]
+        for day_of_week in self.backtest_dates:
+            if day_of_week.weekday() > 3:
+                days_to_add = 7 - (day_of_week.weekday() - 3)
+            else:
+                days_to_add = (3 - day_of_week.weekday()) % 7
+            target_weekly_exp = day_of_week+ dt.timedelta(days=days_to_add)
+            if target_weekly_exp in self.india_holidays:
+                print("Thrusday holiday")
+                target_weekly_exp = target_weekly_exp -dt.timedelta(days=1)
+            target_weekly_exp_formatted.append(target_weekly_exp.strftime('%d%b').upper())
 
-    def next_weekly(self):
-        if self.day_of_week() >= 3:  #If today is Thursday or after
-            days_to_add = 7 - (self.day_of_week() - 3)
-        else:  # If today is before Thursday
-            days_to_add = (3 - self.day_of_week())
-        target_next_weekly_exp = self.current_backtest_date + dt.timedelta(days=days_to_add + 7)
-        if target_next_weekly_exp in self.india_holidays:
-            print("Thrusday holiday")
-            target_next_weekly_exp = target_next_weekly_exp -dt.timedelta(days=1)
-        target_next_weekly_exp_formatted = target_next_weekly_exp.strftime('%d%b').upper()
-        return target_next_weekly_exp_formatted
+        return list(set(target_weekly_exp_formatted))
 
     def monthly(self):
-        next_month = self.current_backtest_date.replace(day=28) + dt.timedelta(days=4)
-        last_day_of_month = next_month - dt.timedelta(days=next_month.day)
-        current_month = last_day_of_month.month
+        monthly_exp_formatted = []
 
-        while last_day_of_month.weekday() != 3:
-            last_day_of_month -= dt.timedelta(days=1)
-        third_thursday = last_day_of_month
+        for date in self.backtest_dates:
+            next_month = date.replace(day=28) + dt.timedelta(days=4)
+            last_day_of_month = next_month - dt.timedelta(days=next_month.day)
+            current_month = last_day_of_month.month
+
+            while last_day_of_month.weekday() != 3:
+                last_day_of_month -= dt.timedelta(days=1)
+
+            monthly_exp = last_day_of_month.strftime('%d%b').upper()
+            monthly_exp_formatted.append(monthly_exp)
 
 
-        if third_thursday.month != current_month:
-            while third_thursday.month != current_month:
-                third_thursday -= dt.timedelta(days=1)
-                third_thursday = third_thursday.strftime('%d%b').upper()
-        third_thursday = third_thursday.strftime('%d%b').upper()
-        return third_thursday
+        return list(set(monthly_exp_formatted))
 
-    def next_monthly(self):
-        next_month_start = self.current_backtest_date.replace(day=1) + pd.DateOffset(months=1)
-        first_thursday = next_month_start + pd.DateOffset(weekday=3)
-        third_thursday = first_thursday + pd.DateOffset(weeks=2)
-        third_thursday_formatted = third_thursday.strftime('%d%b').upper()
-        return third_thursday_formatted
+
+
 
 
 
